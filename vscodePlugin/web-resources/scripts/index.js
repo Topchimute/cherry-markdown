cd47eabb7cc1bd87037c5bfc8815537daa8d68ff
Topchimute <101563593+Topchimute@users.noreply.github.com>
2023-07-07 18:05:27 +0800

    themes

--- vscodePlugin/web-resources/scripts/index.js
+++ vscodePlugin/web-resources/scripts/index.js
@@ -83,6 +83,9 @@ const customMenuChangeTheme = Cherry.createMenuHook('主题',  {
     { noIcon: true, name: '热情·red', onclick: () => {
       cherry.toolbar.menus.hooks.customMenuChangeTheme.fire(null, 'red');
     } },
+    { noIcon: true, name: '潮流·cyber', onclick: () => {
+      cherry.toolbar.menus.hooks.customMenuChangeTheme.fire(null, 'cyber');
+    } },
   ],
 });
 
